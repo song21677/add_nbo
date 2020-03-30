06257055f280fd0d5ad594359b746612119f8fd7
sy99 <song21677@naver.com>
2020-03-30 19:03:58 +0900

    Modified add_nbo.c

--- add_nbo.c
+++ add_nbo.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 void usuage() {
-	printf("syntax : add_nbo <file1> <file2>");
-	printf("sample : add_nbo a.bin c.bin");
+    printf("syntax : add_nbo <file1> <file2>");
+    printf("sample : add_nbo a.bin c.bin");
 }
 
 uint32_t my_ntohl(uint32_t n) {
-    return ((n & 0xFF000000) >> 24 | (n & 0x00FF0000) >> 8 | (n & 0x0000FF00) << 8 | (n & 0x000000FF) << 24);
+    return ((n & 0xFF000000) >> 24 | (n & 0x00FF0000) >> 8 | 
+    (n & 0x0000FF00) <<8 | (n & 0x000000FF) << 24);
+}
+
+uint32_t add_nbo(char filename[]) {
+    FILE *fp;
+    uint32_t buffer;
+    size_t num;
+
+    if ((fp = fopen(filename, "rb")) == NULL) {
+        printf("Can't open file.\n");
+        exit(1);
+    }
+    if ((num = fread(&buffer, 4, 1, fp)) != 1) {
+	    printf("Can't read file.\n");
+	    exit(1);
+    }
+    fclose(fp);
+    
+    buffer = my_ntohl(buffer);
+    
+    return buffer;
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 3) {
-		usuage();
-		return -1;
-	}
-
-	FILE *fp1, *fp2;
-	uint32_t buffer1, buffer2;
-
-	fp1 = fopen(argv[1], "rb");
-	fp2 = fopen(argv[2], "rb");
-	if (fp1 == NULL | fp2 == NULL) {
-		printf("Can't open file.\n");
-		return -1;
-	}
-    	fread(&buffer1, 4, 1, fp1);
-    	fread(&buffer2, 4, 1, fp2);
-	fclose(fp1);
-	fclose(fp2);
-
-    	buffer1 = my_ntohl(buffer1);
-    	buffer2 = my_ntohl(buffer2);
-
-    	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", buffer1, buffer1, buffer2, buffer2, buffer1 + buffer2, buffer1 + buffer2);
+    if (argc != 3) {
+        usuage();
+        return -1;
+    }
+    
+    int i;
+    uint32_t rv[3];
+    for (i=1; i<3; i++) {
+        rv[i] = add_nbo(argv[i]);
+    }
+    
+    printf("%d(%#x) + %d(%#x) = %d(%#x)\n", rv[1], rv[1], 
+    rv[2], rv[2], rv[1] + rv[2], rv[1] + rv[2]);
 }
-
 
